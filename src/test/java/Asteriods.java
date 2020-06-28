1a6133d9a24f1911d91261ee146c371ac545d37e
Earl Bingham <earlbingham@gmail.com>
2020-06-28 12:12:07 -0700

    Added documentation for code I copied from youtube video...

--- src/test/java/Asteriods.java
+++ src/test/java/Asteriods.java
@@ -3,11 +3,12 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Stack;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Asteriods {
 
+    // This ones tied to youtube video: https://www.youtube.com/watch?v=5AV33YdtDYw&t=623s
+    // I'm starting to like him the most regarding coding tutorials
     @Test
     public void shouldGetRemaining() {
         int[] asteroids =  {5, 10, -5};
